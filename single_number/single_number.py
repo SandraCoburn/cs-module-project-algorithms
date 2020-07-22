5534b44b2100c7fda4416bc5a079c9b5cee6b773
Sandra Coburn <sandra_coburn@hotmail.com>
2020-07-22 14:12:15 -0700

    Finished single number, test pass

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,20 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    #check if there is any python built in method to check for duplicates
+    # check arr for index of numbers and single out the one that only shows up once
+    #variable to hold the current number in array
+    #current = arr[0]
+    #variable to hold the numbers that doesn't repeat
 
-    pass
+    for n in arr:
+        #conditional to check if number is repeated
+        current = arr.count(n)
+        if current == 1:
+            duplicate = n
+    return duplicate
+
+    
 
 
 if __name__ == '__main__':
